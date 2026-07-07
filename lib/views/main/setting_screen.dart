0c994fe17a8de56582e5f74c276137f9cbfee278
Yaseen Malik <malik399758@github.com>
2026-07-06 23:35:00 -0700

    update subject.

--- lib/views/main/setting_screen.dart
+++ lib/views/main/setting_screen.dart
@@ -542,8 +542,6 @@ class SettingsScreen extends StatelessWidget {
     );
   }
 }*/
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
